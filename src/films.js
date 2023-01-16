59e72aa9eb635bb89c9a780431206937afdf1caf
abrmoralesm <abrmoralesm@gmail.com>
2023-01-16 16:00:51 +0100

    Exercise 1

--- src/films.js
+++ src/films.js
@@ -1,6 +1,6 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  const result =  array.map(jar=>jar.director)
   console.log("EXERCICE 1 ->", result);
   return result;
 }
