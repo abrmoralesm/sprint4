c5438ee883c4647d01492a9ab63341153924cb49
abrmoralesm <abrmoralesm@gmail.com>
2023-01-25 20:00:03 +0100

    Exercise 5
    Order by year, ascending

--- src/films.js
+++ src/films.js
@@ -1,53 +1,49 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  const result =  array.map(jar=>jar.director)
-  console.log("EXERCICE 1 ->", result);
+  const result = array.map((jar) => jar.director);
+  console.log('EXERCICE 1 ->', result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
-  const result = array.filter(jar=>jar.director===director);
-  console.log("EXERCICE 2 ->", result);
+  const result = array.filter((jar) => jar.director === director);
+  console.log('EXERCICE 2 ->', result);
   return result;
-} 
- 
-// Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {
-  let pelis = array.filter(jar => jar.director == director);
-  let averageScore = pelis.reduce((total,next) => total + next.score,0) / pelis.length;
-  return averageScore;
-  
 }
 
-// Exercise 4:  Alphabetic order by title 
+// Exercise 3: Calculate the average of the films of a given director.
+function moviesAverageOfDirector(array, director) {
+  let pelis = array.filter((jar) => jar.director == director);
+  let averageScore =
+    pelis.reduce((total, next) => total + next.score, 0) / pelis.length;
+  return averageScore;
+}
+
+// Exercise 4:  Alphabetic order by title
 function orderAlphabetically(array) {
-  let getTitulos = array.map(jar=>jar.title);
-  let first20 = getTitulos.sort().slice(0,20);
-  return first20; 
+  let getTitulos = array.map((jar) => jar.title);
+  let first20 = getTitulos.sort().slice(0, 20);
+  return first20;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(array) {
+  let sorted = array
+    .map((item) => item)
+    .sort((a, b) => (a.title > b.title ? 1 : b.title > a.title ? -1 : 0));
+  sorted.sort((a, b) => a.year - b.year);
+  return sorted;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
-}
+function moviesAverageByCategory() {}
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
-
-}
+function hoursToMinutes() {}
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
-}
-
-
+function bestFilmOfYear() {}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -60,6 +56,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     moviesAverageByCategory,
     hoursToMinutes,
-    bestFilmOfYear,
+    bestFilmOfYear
   };
 }
